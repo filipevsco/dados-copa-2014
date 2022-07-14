4add5047f5f1ed9a007835145e1014c844099c8c
Filipe Vasconcelos <filipev@me.com>
2022-07-14 18:00:59 -0300

    fix extrai_zip.py

--- extrai_zip.py
+++ extrai_zip.py
@@ -5,12 +5,13 @@ import zipfile
 
 def main(path):
     if not os.path.exists(path):
-        print(f."Arquivo {path} não existe.")
+        print(f"Arquivo {path} não existe.")
     else:
-        zfile = zipfile.Zipfile(path)
+        zfile = zipfile.ZipFile(path)
         zfile.extractall()
         print("Arquivos extraídos")
         
 
 if __name__ == "__main__":
-    return main(argv[1])
+    main(sys.argv[1])
+
