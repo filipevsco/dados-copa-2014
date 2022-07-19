c11248dc959f3f245c0e8695212b8a3c71b72e13
Filipe Vasconcelos <filipev@me.com>
2022-07-19 08:35:07 -0300

    Save consulta_dados.py.

--- consulta_dados.py
+++ consulta_dados.py
@@ -10,6 +10,13 @@ def main():
     
     # dicionario de relacionamentos
     relationchips = {}
+    
+    for meta_data_file in os.listdir("data/meta-data"):
+        table_name = extract_name(meta_data_file)
+        attributes = read_metadata(meta_data_file)
+        identifier = atributes[0]
+        
+        meta[table_name] = 
 
 if __name__ == "__main__":
     main()
