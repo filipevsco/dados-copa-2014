91a77cbdfa5ebc033364ceb3608cfcac77fb5791
Filipe Vasconcelos <filipev@me.com>
2022-07-19 08:38:28 -0300

    Save consulta_dados.py.

--- consulta_dados.py
+++ consulta_dados.py
@@ -9,14 +9,21 @@ def main():
     keys = {}
     
     # dicionario de relacionamentos
-    relationchips = {}
+    relationships = {}
     
     for meta_data_file in os.listdir("data/meta-data"):
         table_name = extract_name(meta_data_file)
         attributes = read_metadata(meta_data_file)
         identifier = atributes[0]
         
-        meta[table_name] = 
+        meta[table_name] = atributes
+        keys[identifier] = table_name
+        
+    for keys, val in meta.items():
+        for col in val:
+            if col[0] in keys:
+                if not col[0] == meta[key][0][0]:
+                    relationships[key]
 
 if __name__ == "__main__":
     main()
