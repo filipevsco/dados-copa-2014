98ad64a2bcda9c8061cd28d7962311a7b1bc47c0
Filipe Vasconcelos <filipev@me.com>
2022-07-25 06:38:38 -0300

    Save test_datatable.py.

--- test_datatable.py
+++ test_datatable.py
@@ -6,3 +6,6 @@ from domain import DataTable
 class DataTableTest(unittest.TestCase):
     def test_add_column(self):
         self.AssertEqual(0, len(self.table.__columns))
+
+        self.table.add_column('BId', 'bigint')
+        self.AssertEqual(1, len(self.table.__columns))
