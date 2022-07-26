31b083200e1511fbb067e4abb268f5294298608a
Filipe Vasconcelos <filipev@me.com>
2022-07-26 06:34:19 -0300

    Save test_domain.py.

--- test_domain.py
+++ test_domain.py
@@ -1,1 +1,22 @@
 import unittest
+
+from domain import DataTable
+
+
+class DataTableTest(unittest.TestCase):
+    def test_add_column(self):
+        self.AssertEqual(0, len(self.table.__columns))
+
+        self.table.add_column('BId', 'bigint')
+        self.AssertEqual(1, len(self.table.__columns))
+        
+        self.table.add_column('value', 'numeric')
+        self.assertEqual(2, len(self.table.__columns))
+        
+        self.table.add_column('desc','varchar')
+        self.assertEqual(3, len(self.table.__columns))
+    
+    def test_add_column_invalid_type(self):
+        a_table = DataTable('A')
+        
+        self.assertRaises(Exception, a_table.add_column, ('col', 'invalid'))
